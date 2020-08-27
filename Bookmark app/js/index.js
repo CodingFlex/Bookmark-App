c92aad032362af70599f329c6586dbff7656af7b
Oluwatunmise Dunmade <oluwatunmisedunmade2017@gmail.com>
2020-08-27 08:50:47 -0700

    Add files via upload

---
+++ Bookmark app/js/index.js
@@ -0,0 +1,86 @@
+//form submit limit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+//save bookmark
+function saveBookmark(e){
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+    
+    if(!validateForm(siteName, siteUrl)){
+        return false;
+    }
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    if(localStorage.getItem('bookmarks') === null) {
+        var bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+
+    //clear form
+    document.getElementById('myForm').reset();
+
+    fetchBookmarks();
+
+    //Prevent form from submitting
+    e.preventDefault();
+}
+
+function deleteBookmark(url) {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    for(var i = 0 ;i< bookmarks.length;i++){
+        if(bookmarks[i].url == url) {
+            bookmarks.splice(i, 1);
+        }
+    }
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    fetchBookmarks();
+}
+
+
+function fetchBookmarks() {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    var bookmarksResults = document.getElementById('bookmarksResults');
+
+    bookmarksResults.innerHTML = '';
+
+    for(var i = 0; i < bookmarks.length; i++){
+        var name =  bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">'+
+                                      '<h3>'+name+
+            ' <a class="btn btn-default" target="blank" href="' + url +'">Follow <i class=" fas fa-external-link-alt fa-1x text-light"></i></a> ' +
+            ' <a onclick="deleteBookmark(\'' + url +'\')" class="btn btn-danger" href="#">Delete <i class=" fa fa-trash fa-1x text-black"></i></a> '
+                                      '</h3>'+
+                                      '</div>';
+    }
+}
+
+function validateForm(siteName, siteUrl){
+    if (!siteName || !siteUrl) {
+        alert('Form can not be empty')
+        return false;
+    }
+
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if (!siteUrl.match(regex)) {
+        alert('Use a valid Url');
+        return false;
+    }
+
+    return true;
+}
